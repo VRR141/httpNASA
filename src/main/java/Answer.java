2c58b81ccc3d6b165de3dcb249a052661d906adf
Vladislav <v.r.ryazantsev@gmail.com>
2022-09-28 21:14:59 +0300

    fix remarks

--- src/main/java/Answer.java
+++ src/main/java/Answer.java
@@ -16,8 +16,8 @@ public class Answer {
             @JsonProperty("date") String date,
             @JsonProperty("explanation") String explanation,
             @JsonProperty("hdurl") String hdurl,
-            @JsonProperty("media_type") String media_type,
-            @JsonProperty("service_version") String service_version,
+            @JsonProperty("media_type") String mediaType,
+            @JsonProperty("service_version") String serviceVersion,
             @JsonProperty("title") String title,
             @JsonProperty("url") String url
     ){
@@ -25,8 +25,8 @@ public class Answer {
         this.date = date;
         this.explanation = explanation;
         this.hdurl = hdurl;
-        this.mediaType = media_type;
-        this.serviceVersion = service_version;
+        this.mediaType = mediaType;
+        this.serviceVersion = serviceVersion;
         this.title = title;
         this.url = url;
     }
